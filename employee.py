5b32b3c9253e1cd5f99fa699f68b3a6796c2c6bd
tawoods-2 <tawoods2@crimson.ua.edu>
2026-06-30 13:47:39 -0500

    Update employee.py

--- employee.py
+++ employee.py
@@ -1,8 +1,21 @@
 class Employee:
-    def __init__(self,fname,lname,salary):
+    def __init__(self,fname,lname):
         self.fname = fname
         self.lname = lname
+
+class SalaryEmployee(Employee):
+    def __init__(self,fname,lname,salary):
+        super().__init__(fname,lname)
         self.salary = salary
         
     def calculate_paycheck(self):
         return self.salary/52
+        
+class HourlyEmployee(Employee):
+    def __init__(self,fname,lname,weekly_hours,hourly_rate):
+        super().__init__(fname,lname)
+        self.weekly_hours = weekly_hours
+        self.hourly_rate = hourly_rate
+        
+    def calculate_paycheck(self):
+        return self.weekly_hours*self_hourlyrate
