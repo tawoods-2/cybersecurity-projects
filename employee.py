6a84b04e63ef05fe1fb3bf53537c7faaaff79848
tawoods-2 <tawoods2@crimson.ua.edu>
2026-06-30 13:53:25 -0500

    Update employee.py

--- employee.py
+++ employee.py
@@ -19,3 +19,14 @@ class HourlyEmployee(Employee):
         
     def calculate_paycheck(self):
         return self.weekly_hours*self_hourlyrate
+        
+class CommissionEmployee(SalaryEmployee):
+    def __init__(self,fname,lname,salary,sales_num,com_rate):
+        super().__init__(fname,lname,salary)
+        self.sales_num = sales_num
+        self.com_rate = com_rate
+        
+    def calculate_paycheck(self):
+        regular_salary = super().calculate_paycheck()
+        total_commission = self.sales_num*self.com_rate
+        return regular_salary + total_commission
